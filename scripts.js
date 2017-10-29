0c1e70dc561014ff5bbe0d2669c8515708fd1cb0
CharlotteMoriarty <31165670+CharlotteMoriarty@users.noreply.github.com>
2017-10-29 18:06:07 +0100

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,15 @@
+//obliczanie na stronie
+function triangleArea() {
+    var a=document.getElementById('a').value;
+    var h=document.getElementById('h').value;
+    var triangleArea= a*h/2;
+    var wynik =document.getElementById('resoult').innerHTML =triangleArea;
+    wynik;
+}
+//obliczanie na sztywnych danych z pliku +console.log
+/*
+a=5;
+h=6;
+var triangleArea= a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+*/
